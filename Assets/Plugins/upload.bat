7ff37cf46475880bfa9888c06b1f2969b2e57c13
Alex W <alex@metafaux.com>
2021-07-01 19:12:42 -0700

    increase scale, upload to Altspace

---
+++ Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\Source\unity\Flight_to_Mars_2021\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1770429627995521290.json
